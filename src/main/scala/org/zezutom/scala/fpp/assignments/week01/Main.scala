399f937a6c58360c28f8640a39d11df05a129a12
zezutom <zezulatomas@gmail.com>
2012-12-16 21:22:39 +0100

    Added task descriptions.

--- src/main/scala/org/zezutom/scala/fpp/assignments/week01/Main.scala
+++ src/main/scala/org/zezutom/scala/fpp/assignments/week01/Main.scala
@@ -12,7 +12,16 @@ object Main {
   }
 
   /**
-   * Exercise 1
+   * Exercise 1: Pascal's Triangle
+   * 
+   * The numbers at the edge of the triangle are all 1, and each number inside the triangle 
+   * is the sum of the two numbers above it. Write a function that computes the elements of 
+   * Pascal’s triangle by means of a recursive process.
+   * 
+   * Do this exercise by implementing the pascal function in Main.scala, which takes a column c 
+   * and a row r, counting from 0 and returns the number at that spot in the triangle. 
+   * 
+   * For example, pascal(0,2)=1, pascal(1,2)=2 and pascal(1,3)=3.
    */
   def pascal(c: Int, r: Int): Int = {
     if (c == 0 || r == 0 || c == r) 1
@@ -20,7 +29,23 @@ object Main {
   }
 
   /**
-   * Exercise 2
+   * Exercise 2: Parentheses Balancing
+   * 
+   * Write a recursive function which verifies the balancing of parentheses in a string, 
+   * which we represent as a List[Char] not a String. For example, the function should 
+   * return true for the following strings:
+   * 
+   * (if (zero? x) max (/ 1 x))
+   * I told him (that it’s not (yet) done). (But he wasn’t listening)
+   * 
+   * The function should return false for the following strings:
+   * 
+   * :-)
+   * ())(
+   *
+   * The last example shows that it’s not enough to verify that a string contains the same 
+   * number of opening and closing parentheses.
+   * 
    */
   def balance(chars: List[Char]): Boolean = {
 
@@ -45,7 +70,14 @@ object Main {
   }
 
   /**
-   * Exercise 3
+   * Exercise 3: Counting Change
+   * 
+   * Write a recursive function that counts how many different ways you can make 
+   * change for an amount, given a list of coin denominations. For example, there 
+   * are 3 ways to give change for 4 if you have coins with denomiation 1 and 2: 1+1+1+1, 1+1+2, 2+2.
+   * 
+   * Do this exercise by implementing the countChange function in Main.scala. This function takes 
+   * an amount to change, and a list of unique denominations for the coins. 
    */
   def countChange(money: Int, coins: List[Int]): Int = {
     if (money == 0) 1
